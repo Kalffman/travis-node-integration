131c2c45fd31e71c11ddb60dcf52609397dba233
Jefferson Kalffman de Sousa Lacerda <jefferson@kalffman.com>
2021-02-03 06:45:05 -0400

    feat: Consultando variável de ambiente .USER

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
+const user = process.env.USER;
 async function main() {
   while(true) {
     console.log('Containers rule!');
+    console.log(`#${user}TaOn`);
     await sleep(5000);
   }
 }
